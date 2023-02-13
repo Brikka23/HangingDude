88fda3dd5ed8cb313c443a24466227e259aa406d
Brikka23 <ilyabogdanov787@gmail.com>
2023-02-13 23:41:27 +0300

    added UnHook, colorChanger and hook on other Rope

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _player;
+    [SerializeField] private float _forcePush;
+
+    private SpriteRenderer _renderer;
+    private HingeJoint2D _joint;
+    private Rigidbody2D _lastPartOfRope;
+
     private void Start()
     {
-        _player.AddForce(Vector3.left * 125, ForceMode2D.Impulse);
+        _renderer = GetComponent<SpriteRenderer>();
+        _joint = GetComponent<HingeJoint2D>();
+        _player.AddForce(Vector3.left * _forcePush, ForceMode2D.Impulse);
     }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && _joint.connectedBody!=null)
+        {
+            _joint.connectedBody = null;
+            _joint.enabled = false;
+        }
+        changeColorPlayer();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _joint.enabled = true;
+            _lastPartOfRope = collision.gameObject.GetComponent<Rigidbody2D>();
+            _joint.connectedBody = _lastPartOfRope;
+            _player.AddForce(Vector3.right * _forcePush, ForceMode2D.Impulse);
+        }
+    }
+
+    private void changeColorPlayer()
+    {
+        _renderer.color = Color.Lerp(Color.red, Color.green, Mathf.Sin(Time.timeSinceLevelLoad));
+    }
+
+
 }
