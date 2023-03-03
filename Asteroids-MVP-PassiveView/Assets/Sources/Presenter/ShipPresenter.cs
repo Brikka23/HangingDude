308ff1aa24ea61b6d9372f4f2573485e7a1069db
Brikka23 <ilyabogdanov787@gmail.com>
2023-03-04 01:05:33 +0300

    asteroids

---
+++ Asteroids-MVP-PassiveView/Assets/Sources/Presenter/ShipPresenter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ShipPresenter : Presenter
+{
+    private Root _init;
+    private uint _quantityOfLifePlayer = 3;
+
+    public void Init(Root init)
+    {
+        _init = init;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _quantityOfLifePlayer--;
+            if(_quantityOfLifePlayer == 0)
+            {
+                _init.DisableShip();
+            }
+        }
+    }
+}
